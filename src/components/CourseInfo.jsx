2262df09d43d92ae17d34f75ea594a2c2d394db4
L-David-26 <davidlez2mgk@gmail.com>
2021-09-07 11:06:29 -0500

    entrar

---
+++ src/components/CourseInfo.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+const CourseInfo = ()=>{
+    return (
+        <div className="course__Info">
+            <h1>
+                Practing the using React
+            </h1>
+        </div>
+    )
+}
+
+export default CourseInfo
