7999c1d5a10e5441c063dc182398cb86282a12ee
L-David-26 <davidlez2mgk@gmail.com>
2021-09-07 20:11:15 -0500

    entrar

--- src/components/Render.jsx
+++ src/components/Render.jsx
@@ -21,21 +21,34 @@ const RenderJS = () => {
       color: "red",
     },
   ];
-
+  const CardNote = ({ note }) => {
+    return (
+      <div key={note.date}>
+        <h4 style={{ color: note.color }}>{note.conter}</h4>
+        <span>{note.id} </span>
+        <p>{note.date}</p>
+        <h5>{note.state ? "hola soy importate" : "no soy importante"}</h5>
+        <hr />
+      </div>
+    );
+  };
   return (
     <div>
       <h2>Renderizado de Listado y Formularios</h2>
-      {typeof notes === "undefined"
-        ? "the value is undefined or null"
-        : notes.map(({ id, conter, date, state, color }) => (
-            <div key={date}>
-              <h4 style={{ color: color }}>{conter}</h4>
-              <span>{id} </span>
-              <p>{date}</p>
-              <h5>{state ? "hola soy importate" : "no soy importante"}</h5>
-              <hr />{" "}
-            </div>
-          ))}
+      {
+        typeof notes === "undefined"
+          ? "the value is undefined or null"
+          : notes.map((note) => <CardNote note={note} />)
+        // notes.map(({ id, conter, date, state, color }) => (
+        //     <div key={date}>
+        //       <h4 style={{ color: color }}>{conter}</h4>
+        //       <span>{id} </span>
+        //       <p>{date}</p>
+        //       <h5>{state ? "hola soy importate" : "no soy importante"}</h5>
+        //       <hr />{" "}
+        //     </div>
+        //   ))
+      }
     </div>
   );
 };
