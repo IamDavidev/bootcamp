a8deedcb599893630db29166877d8aa26bf735ae
L-David-26 <davidlez2mgk@gmail.com>
2021-09-09 21:04:11 -0500

    using new api ful rest store

--- src/components/Fetch.jsx
+++ src/components/Fetch.jsx
@@ -1,22 +1,28 @@
 import { useState, useEffect } from "react";
 import RenderFetch from "./RenderFetch";
 const Fetch = () => {
-  const [news, setnews] = useState([]);
+  const [news, setNews] = useState([]);
   //27ed20020309422ca0b45da73e1e7893
   useEffect(() => {
-   fetch(
+    fetch(
       "https://newsapi.org/v2/everything?q=tesla&from=2021-08-09&sortBy=publishedAt&apiKey=27ed20020309422ca0b45da73e1e7893"
     )
-      .then((res) => res.json())
+     .then((res) => res.json())
       .then((response) => {
-        const dataNew = response
-        const {content , title , url , publishedAt } = dataNew
-        const {urlToImage} = dataNew
-        return {content , title , url , publishedAt}
-    }); 
-    
+        const { articles } = response;
+        const arti = articles.map((i) => {
+          const { content, title, url, publishedAt } = i;
+          const { urlToImage } = i;
+          return { content, title, url, publishedAt, urlToImage };
+        });
+        return setNews(arti)
+      });
   }, []);
-  return <RenderFetch />;
+  return <RenderFetch props={news}/>;
 };
-// publishedAt urlToImage
 export default Fetch;
+// publishedAt urlToImage
+// const {content , title , url , publishedAt } = dataNew
+// console.log(content)
+// const {urlToImage} = dataNew
+// return setNews({})
--- src/components/RenderFetch.jsx
+++ src/components/RenderFetch.jsx
@@ -1,13 +1,21 @@
-
-const RenderFetch = ()=>{
-
-
-    return (
-        <h1>
-            hola como estas 
-        </h1>
-    )
-}
-
+const RenderFetch = ({ props }) => {
+  return (
+    <>
+      {props.map(({ content, title, url, publishedAt, urlToImage }) => (
+        <>
+          <img src={urlToImage} key={url} alt={content} />
+          <h4>{title}</h4>
+          <p>
+              {content}
+          </p>
+          <strong>
+              {publishedAt}
+          </strong>
+          <hr/>
+        </>
+      ))}
+    </>
+  );
+};
 
 export default RenderFetch;
